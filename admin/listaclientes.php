ff443be6f7a3aa8c7942a8ef7861a7bf733102b9
Fernando Lopes <50033985+finandolopes@users.noreply.github.com>
2024-05-15 15:18:02 -0300

    Atualizar o listaclientes.php

--- admin/listaclientes.php
+++ admin/listaclientes.php
@@ -1,16 +1,5 @@
-﻿<?php
-// Conexão com o banco de dados
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "confinter";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verifica a conexão
-if ($conn->connect_error) {
-    die("Erro na conexão: " . $conn->connect_error);
-}
+<?php
+include_once('../php/conexao.php');
 
 // Consulta SQL para buscar os clientes
 $sql = "SELECT r.id_cliente, c.nome AS nome_cliente, c.email, c.telefone FROM clientes r INNER JOIN clientes c ON r.id_cliente = c.id_cliente";
